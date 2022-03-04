c733fa3b811c697a6c315464c303de2191ee8161
Blaise Hala <blaisehala@gmail.com>
2022-03-04 19:08:01 +0300

    st:img addition and deletion-This commit adds and deletes different files and images

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+let maleNames =[ 'Kwasi','Kwadwo', 'Kwabena','Kwaku','Yaw','Kofi','kwame']
+
+let femaleNames= ['Akosua','Adwoa','Abenaa','Akua','Yaa', 'Afua','Ama']
+
+let Days = ['Sunday','Monday', 'Tuesday', 'Wednesda','Thursday','Friday','Saturday']
