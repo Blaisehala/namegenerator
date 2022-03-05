0e187156631b18ecd32a8c66644f8a337a1018b3
Blaise Hala <blaisehala@gmail.com>
2022-03-06 00:31:16 +0300

    st:Array addition

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let maleNames =[ 'Kwasi','Kwadwo', 'Kwabena','Kwaku','Yaw','Kofi','kwame']
+
+let femaleNames= ['Akosua','Adwoa','Abenaa','Akua','Yaa', 'Afua','Ama']
+
+let Days = ['Sunday','Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday']
 
 
 
