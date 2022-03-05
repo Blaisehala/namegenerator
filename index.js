a277f37aa0cc73774771e9b1cb99089baa30454b
Blaise Hala <blaisehala@gmail.com>
2022-03-05 14:28:55 +0300

    st:Enhanced styling of inputs in the generate name page

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ let maleNames =[ 'Kwasi','Kwadwo', 'Kwabena','Kwaku','Yaw','Kofi','kwame']
 let femaleNames= ['Akosua','Adwoa','Abenaa','Akua','Yaa', 'Afua','Ama']
 
 let Days = ['Sunday','Monday', 'Tuesday', 'Wednesda','Thursday','Friday','Saturday']
+
+
