c50ce9f257104c0c06eb04e1f5d1f7075faf2740
Blaise Hala <blaisehala@gmail.com>
2022-03-06 00:29:13 +0300

    build:Initial addition of javascript function

--- index.js
+++ index.js
@@ -1,9 +1,74 @@
 
 
-let maleNames =[ 'Kwasi','Kwadwo', 'Kwabena','Kwaku','Yaw','Kofi','kwame']
-
-let femaleNames= ['Akosua','Adwoa','Abenaa','Akua','Yaa', 'Afua','Ama']
-
-let Days = ['Sunday','Monday', 'Tuesday', 'Wednesda','Thursday','Friday','Saturday']
 
 
+
+let form = document.getElementById('sbb');
+form.addEventListener('click', function(event){
+
+event.preventDefault();
+
+calling();
+
+});
+
+
+function calling() {
+
+  let dateOfBirth = document.getElementById('datedate').value;
+
+  var z = document.getElementById('para');
+  z.innerHTML = "your Akan Name is" + " " + dateOfBirth ;
+
+  console.log(dateOfBirth);
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// const calender = document.getElementById('datedate').value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // invaliddate
+// if ( month <=0 || month >12){
+//   alert('Invalid Date')
+// };
